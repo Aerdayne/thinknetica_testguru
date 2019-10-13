dbda7d0d122377d235ed3e61e64edcfd15e191fc
Aerdayne <daenskus@gmail.com>
2019-10-13 13:27:09 +0300

    FK constraints

--- db/migrate/20191011113348_add_association_columns.rb
+++ db/migrate/20191011113348_add_association_columns.rb
@@ -1,6 +1,6 @@
 class AddAssociationColumns < ActiveRecord::Migration[6.0]
   def change
-    add_reference :answers, :question
-    add_reference :questions, :test
+    add_reference :answers, :question, foreign_key: true
+    add_reference :questions, :test, foreign_key: true
   end
 end
